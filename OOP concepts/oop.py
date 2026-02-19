3f552a4f4c08459a7f62eac906c193620df730ca
Yogin N C <yoginnc@gmail.com>
2026-02-19 19:50:42 +0530

    first class program

--- OOP concepts/oop.py
+++ OOP concepts/oop.py
@@ -1,1 +1,15 @@
-print("Welcome to Python OOP Learning")
+#print("Welcome to Python OOP Learning")
+
+class Mobile:
+    def __init__(self,brand,price):
+        self.brand=brand
+        self.price=price
+
+    def Display(self):
+        print(f"{self.brand} costs {self.price}")
+
+M1=Mobile("Iphone",83000)
+M2=Mobile("Relame",15000)
+
+M1.Display()
+M2.Display()
